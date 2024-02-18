9629db6df3687ef88caefb2e212f3cc58666a23f
kmc369 <keishacoleman710@gmail.com>
2024-02-17 21:21:51 -0500

    side bar continued

--- react-app/src/components/SideBar/index.js
+++ react-app/src/components/SideBar/index.js
@@ -10,8 +10,8 @@ export default function SideBar(){
     <>
     <div className='SideBar-Container'>
             <div className='Home-Container'>
-                <div><i class="fa-solid fa-music" style={{color: "white"}}><span style={{color: "rgb(33, 197, 33)"}} className="sidebar-words">Slotify</span></i></div>
-                <div><i class="fa-solid fa-house" style={{color: "#ffffff"}}><span className="sidebar-words">Home</span></i></div>
+                <div><i class="fa-solid fa-music" style={{ color: "white" }}><span style={{color: "rgb(33, 197, 33)",paddingLeft: 10}} className="sidebar-words">Slotify</span></i></div>
+                <div><i class="fa-solid fa-house" style={{color: "#ffffff"}}><span className="sidebar-words" style={{paddingLeft: 10}}>Home</span></i></div>
 
             </div>
 
