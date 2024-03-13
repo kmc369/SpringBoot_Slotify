1e37f03f87e6a863c30f19814a6ac87852e8ff9e
kmc369 <keishacoleman710@gmail.com>
2024-03-13 09:48:11 -0400

    side bar hovers

--- react-app/src/components/SideBar/index.js
+++ react-app/src/components/SideBar/index.js
@@ -10,13 +10,13 @@ export default function SideBar(){
     <>
     <div className='SideBar-Container'>
             <div className='Home-Container'>
-                <div><i class="fa-solid fa-music" style={{ color: "white" }}><span style={{color: "rgb(33, 197, 33)",paddingLeft: 10}} className="sidebar-words">Slotify</span></i></div>
-                <div><i class="fa-solid fa-house" style={{color: "#ffffff"}}><span className="sidebar-words" style={{paddingLeft: 10}}>Home</span></i></div>
+                <div className='music-container'><i class="fa-solid fa-music" style={{ color: "white" }}><span style={{color: "rgb(33, 197, 33)",paddingLeft: 10}} className="sidebar-words">Slotify</span></i></div>
+                <div className='house-container'><i class="fa-solid fa-house" style={{color: "#ffffff"}}><span className="sidebar-words" style={{paddingLeft: 10}}>Home</span></i></div>
 
             </div>
 
             <div className='Playlist-container'>
-            <div><i class="fa-solid fa-book" style={{color: "#ffffff"}}><span  className="sidebar-words" style={{paddingLeft: 10}}>Library</span></i></div>
+            <div className='library-container'><i class="fa-solid fa-book" style={{color: "#ffffff"}}><span  className="sidebar-words" style={{paddingLeft: 10}}>Library</span></i></div>
             
 
             </div>
